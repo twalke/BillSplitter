b4297938fb12a47db06cc747bb8531e2f0665139
twalke <tyler.walke@gmail.com>
2017-11-12 02:06:34 -0700

    Cannot get login to launch first.

--- app/src/main/java/com/example/twalke/billsplitter/LauncherActivity.java
+++ app/src/main/java/com/example/twalke/billsplitter/LauncherActivity.java
@@ -1,6 +1,33 @@
 package com.example.twalke.billsplitter;
 
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.provider.SyncStateContract.Constants;
+
 
 public class LauncherActivity extends AppCompatActivity {
+
+    private boolean firstLaunch = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launcher);
+        Intent i;
+
+       // SharedPreferences pref = getSharedPreferences(Constants.ApplicationTag, MODE_PRIVATE);
+       // firstLaunch = pref.getBoolean(Constants.FIRST_LAUNCH, true);
+
+        if (firstLaunch) {
+            i = new Intent(LauncherActivity.this, LoginActivity.class);
+            startActivity(i);
+        } else {
+            i = new Intent(LauncherActivity.this, MainActivity.class);
+            startActivity(i);
+        }
+
+        finish();
+    }
 }
